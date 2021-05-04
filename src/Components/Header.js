3314e4bba4e8db610b2be0058a907855ba9f2dab
arsenhuk_oleh <aresnhykoleh@gmail.com>
2021-05-05 00:29:17 +0300

    add header

---
+++ src/Components/Header.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react'
+
+import {AppBar, Container, IconButton, Toolbar, Typography, Box} from '@material-ui/core';
+
+
+
+import SearchIcon from '@material-ui/icons/Search';
+
+/* import SearchBar from "material-ui-search-bar"; */
+
+import {withStyles} from '@material-ui/core/styles';
+
+import logo from "../image/logo.png";
+
+
+import {header as styles} from '../Styles';
+
+
+class Header extends Component{
+    render(){
+        const { classes } = this.props;
+
+        return (
+            <AppBar position="fixed">
+
+                <Container fixed>
+
+                    <Toolbar>   
+
+                    <Typography className={classes.title}> 
+
+                        <img src={logo} className={classes.logo} alt="logo" />
+
+                    </Typography> 
+
+
+
+                    <Box>
+
+                        <IconButton color="inherit" className={classes.search}>
+
+                        <SearchIcon />
+
+                        {/* <SearchBar
+                        value={this.state.value}
+                        onChange={(newValue) => this.setState({ value: newValue })}
+                        onRequestSearch={() => doSomethingWith(this.state.value)}
+                        /> */}
+
+                        </IconButton>
+
+                    </Box> 
+
+                    </Toolbar>
+
+                    
+
+                </Container>
+
+                
+            </AppBar>
+        )
+    }
+
+}
+
+
+
+
+export default withStyles(styles)(Header);
---
+++ src/Styles.js
@@ -0,0 +1,16 @@
+export const header = theme => ({
+
+    root: {
+        flexGrow: 1,
+    },
+    logo: {
+        margin: 0
+      },
+      search: {
+        marginLeft: theme.spacing(1)
+      },
+      title: {
+        flexGrow:1
+      }
+
+})
